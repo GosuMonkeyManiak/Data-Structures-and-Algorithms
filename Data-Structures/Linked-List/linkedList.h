9bd3aa2569e0fe7fc7287b1342b4a9ae5101078d
Dimitar Kupanov <dimitarkupanov@gmail.com>
2024-01-31 22:02:18 +0200

    Added code guards in header files

--- Data-Structures/Linked-List/linkedList.h
+++ Data-Structures/Linked-List/linkedList.h
@@ -1,3 +1,7 @@
+#ifndef LINKEDLIST
+
+#define LINKEDLIST
+
 extern void configList(void * (*_nextElementFunc)(void *currentElement), 
                    void (*_setPointerFieldFunc)(void *currentElement, void *newElement),
                    int (*_deleteConditionFunc)(void *currentElement, void *data),
@@ -8,3 +12,5 @@ extern int deleteByCondition(void **head, void *data);
 extern int deleteFirst(void **head);
 extern int deleteLast(void **head);
 extern void printList(void *head);
+
+#endif
--- Data-Structures/Queue/queue.h
+++ Data-Structures/Queue/queue.h
@@ -1,3 +1,9 @@
+#ifndef QUEUE
+
+#define QUEUE
+
 extern void configQueue(void * (*_nextElementFunc)(void *currentElement), void (*_setPointerFieldFunc)(void *currentElement, void *newElement));
 extern void enqueue(void **head, void *newElement);
 extern int dequeue(void **head);
+
+#endif
